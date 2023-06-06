4f2236481f528f3b03b06bb89cf44dcf0c91840a
BETHWEL <bethwelk85@gmail.com>
2023-06-06 12:36:32 +0300

    avarage done

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,44 +1,102 @@
-from sqlalchemy import create_engine, Column, Integer, String, func
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-from Review import Review
+class Customer:
+    def __init__(self, given_name, family_name):
+        self._given_name = given_name
+        self._family_name = family_name
 
-# Create the SQLAlchemy engine and session
-engine = create_engine('sqlite:///restaurants.db')
-Session = sessionmaker(bind=engine)
-session = Session()
 
-# Base class for declarative models
-Base = declarative_base()
 
-class Restaurant(Base):
-    __tablename__ = 'restaurants'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    def given_name(self):
+        return self._given_name
 
-    reviews = relationship('Review', back_populates='restaurants')
-    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+    def family_name(self):
+        return self._family_name
 
+    def full_name(self):
+        return f"{self._given_name} {self._family_name}"
+
+
+class Review:
+    all_reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+        Review.all_reviews.append(self)
+
+    def rating(self):
+        return self._rating
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
+
+    @classmethod
+    def all(cls):
+        return cls.all_reviews
+
+    def __str__(self):
+        return f"Review by {self._customer.full_name()} for {self._restaurant.name()}: Rating {self._rating}"
+
+
+class Restaurant:
     def __init__(self, name):
         self._name = name
+        self._reviews = []
 
-    @property
     def name(self):
         return self._name
-    
+
+    def add_review(self, review):
+        self._reviews.append(review)
+
     def reviews(self):
-        return self.reviews
-    
+        return self._reviews
+
     def customers(self):
         unique_customers = set()
-        for review in self.reviews:
-            unique_customers.add(review.customer)
+        for review in self._reviews:
+            unique_customers.add(review.customer().full_name())
         return list(unique_customers)
-    
+
     def average_star_rating(self):
-        rating_sum= session.query(func.sum(Review.rating)).filter(Review.restaurant_id == self.id).scalar()
-        rating_count = session.query(func.count(Review.rating)).filter(Review.restaurant_id == self.id).scalar()
-        if rating_count:
-            return rating_sum / rating_count
+        total_ratings = 0
+        for review in self._reviews:
+            total_ratings += review.rating()
+        if len(self._reviews) > 0:
+            return total_ratings / len(self._reviews)
         else:
-            return None
+            return 0.0
+
+
+
+customer1 = Customer("Mzee", "Bikes")
+customer2 = Customer("Mtu", "Kutu")
+customer3 = Customer("Heri", "Wewe")
+
+restaurant = Restaurant("KFC")
+# restaurant1 = Restaurant("Kibandaski")
+
+
+review1 = Review(customer1, restaurant, 5)
+review2 = Review(customer2, restaurant, 3)
+# review3 = Review(customer3, restaurant)
+
+restaurant.add_review(review1)
+restaurant.add_review(review2)
+# restaurant.add_review(review3)
+
+all_reviews = restaurant.reviews()
+for review in all_reviews:
+    print(review)
+
+
+unique_customers = restaurant.customers()
+print(unique_customers)
+
+
+
+average_rating = restaurant.average_star_rating()
+print (f"THE AVARAGE RATING IS {average_rating}")
